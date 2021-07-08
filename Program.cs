22dc0bbf4937098bbd877beb5f9ffde3ad0c1c11
RitchielRez <ritchielrez@gmail.com>
2021-07-08 12:31:22 +0600

    Updated

--- Program.cs
+++ Program.cs
@@ -2,40 +2,40 @@
 
 class Basic
 {
-		static void Basics()
-		{
-				string Name = Console.ReadLine();
+    static void Basics()
+    {
+        string Name = Console.ReadLine();
 
-    		Console.WriteLine("Hello, {0}", Name);
-				
-		}
+        Console.WriteLine("Hello, {0}", Name);
 
-		static void Vars()
-		{
-				double num1, num2, num3;
+    }
 
-				Console.Write("Enter a number: ");
-				num1 = Convert.ToDouble(Console.ReadLine());
+    static void Vars()
+    {
+        double num1, num2, num3;
 
-				Console.Write("Enter another number: ");
-				num2 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter a number: ");
+        num1 = Convert.ToDouble(Console.ReadLine());
 
-				Console.Write("Enter another number: ");
-				num3 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter another number: ");
+        num2 = Convert.ToDouble(Console.ReadLine());
 
-				double total = num1 + num2 + num3;
+        Console.Write("Enter another number: ");
+        num3 = Convert.ToDouble(Console.ReadLine());
 
-				Console.WriteLine("The average is {0}", total / 3);
+        double total = num1 + num2 + num3;
 
-		}
+        Console.WriteLine("The average is {0}", total / 3);
 
-		static void Main()
-		{
-    		Console.Title = "BasicApp";   
+    }
 
-				Console.ForegroundColor = ConsoleColor.Green;
+    static void Main()
+    {
+        Console.Title = "BasicApp";
 
-				Basics();
-				Vars();
-		}
+        Console.ForegroundColor = ConsoleColor.Green;
+
+        Basics();
+        Vars();
+    }
 }
